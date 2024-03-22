8c411f2ffc104605b9e413abc0cfaaa59a3b6d3d
arthur <arthur.rios007@gmail.com>
2024-03-22 11:25:22 -0300

    chore: Add styles and fonts

---
+++ src/styles/globals.ts
@@ -0,0 +1,15 @@
+import { globalCss } from '@arthurrios-ignite-ui/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+    backgroundColor: '$gray-900',
+    color: '$gray-100',
+    '-webkit-font-smoothing': 'antialiased',
+  }
+})
