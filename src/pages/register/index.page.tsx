f39bf2f0966dd17b821c8ce992b8ba17653f820a
arthur <arthur.rios007@gmail.com>
2024-03-22 15:25:12 -0300

    chore: Save user id cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -13,6 +13,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -54,7 +55,10 @@ export default function Register() {
         username: data.username,
       })
     } catch (error) {
-      console.log(error)
+      if (error instanceof AxiosError && error?.response?.data?.message) {
+        alert(error.response.data.message)
+      }
+      console.error(error)
     }
   }
 
