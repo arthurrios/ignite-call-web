5e29e83036e5ec0a467a253c6ec57aa8390945f9
arthur <arthur.rios007@gmail.com>
2024-03-27 14:35:43 -0300

    chore: Fix prisma schema and add adapter methods

--- prisma/migrations/20240327171658_create_auth_tables/migration.sql
+++ prisma/migrations/20240327171658_create_auth_tables/migration.sql
@@ -13,13 +13,15 @@ CREATE TABLE "accounts" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "user_id" TEXT NOT NULL,
     "provider_type" TEXT NOT NULL,
-    "provider_id" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
     "provider_account_id" TEXT NOT NULL,
     "refresh_token" TEXT,
     "access_token" TEXT,
-    "access_token_expires" DATETIME,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
     CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -29,18 +31,14 @@ CREATE TABLE "sessions" (
     "user_id" TEXT NOT NULL,
     "expires" DATETIME NOT NULL,
     "session_token" TEXT NOT NULL,
-    "access_token" TEXT NOT NULL,
     CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "accounts_provider_id_provider_account_id_key" ON "accounts"("provider_id", "provider_account_id");
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
-
--- CreateIndex
-CREATE UNIQUE INDEX "sessions_access_token_key" ON "sessions"("access_token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
