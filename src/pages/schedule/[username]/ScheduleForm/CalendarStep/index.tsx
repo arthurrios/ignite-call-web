a4a15658fe1722203c391ff19148125317357bfd
arthur <arthur.rios007@gmail.com>
2024-03-30 11:07:52 -0300

    feat: Add time picker component

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            tuesday <span>september 20th</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>8:00 AM</TimePickerItem>
+            <TimePickerItem>9:00 AM</TimePickerItem>
+            <TimePickerItem>10:00 AM</TimePickerItem>
+            <TimePickerItem>11:00 AM</TimePickerItem>
+            <TimePickerItem>12:00 AM</TimePickerItem>
+            <TimePickerItem>1:00 PM</TimePickerItem>
+            <TimePickerItem>2:00 PM</TimePickerItem>
+            <TimePickerItem>3:00 PM</TimePickerItem>
+            <TimePickerItem>4:00 PM</TimePickerItem>
+            <TimePickerItem>5:00 PM</TimePickerItem>
+            <TimePickerItem>6:00 PM</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
