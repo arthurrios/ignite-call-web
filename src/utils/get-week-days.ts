14e22d4d17ffa3813410d68b9830a8e495e6fbef
arthur <arthur.rios007@gmail.com>
2024-03-30 10:13:40 -0300

    feat: Add calendar visual structure

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,7 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
-  return Array.from(Array(7).keys()).map((day) =>
+  const weekDays = Array.from(Array(7).keys()).map((day) =>
     formatter.format(new Date(Date.UTC(2021, 5, day))),
   )
+
+  if (short) {
+    return weekDays.map((weekDay) => weekDay.substring(0, 3).toUpperCase())
+  }
+
+  return weekDays
 }
